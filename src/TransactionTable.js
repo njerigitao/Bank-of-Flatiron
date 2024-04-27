177537c866d73ad32e04bc27a12f532ffe87243d
AGNESGITAU <agnesgitau40@gmail.com>
2024-04-27 18:01:12 +0300

    Bank-creatingTable

---
+++ src/TransactionTable.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function TransactionTable(){
+    return (
+        <div>
+            <h1>The Royal Bank of Flatiron</h1>
+            <button>Search your Recent Transaction</button>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Date</th>
+                        <th>Description</th>
+                        <th>Category</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                 <td></td>
+                </tbody>
+            </table>
+        </div>
+    )
+}
+export default TransactionTable;
