03b2a824b269a73990d5a2ac34c2aff746b36021
AGNESGITAU <agnesgitau40@gmail.com>
2024-04-27 19:27:14 +0300

    Bank filter transactions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import TransactionTable from "./TransactionTable";
 function App() {
+  const [transactions, setTransactions] =useState([]);
+  const addTransaction =(newTransaction)=> {
+    setTransactions([...transactions, newTransaction]);
+  };
+  const handleSearch = (searchTerm) => {
+    const filteredTransactions= transactions.filter(transaction=> transaction.description.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+  return filteredTransactions;
+  }
   return (
     <div className="App">
      <TransactionTable />
